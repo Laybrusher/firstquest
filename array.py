c05adc120c71775d822e3133d5280da59855601f
Artoker <savvin.michil@mail.ru>
2020-11-19 21:55:10 +0900

    часть кода связанная с выводом массива

--- array.py
+++ array.py
@@ -7,3 +7,9 @@ for volley in range(volley2):
     string = ""
     if volley % 2:
         mass_1.insert(0, volley)
+for zero in range(volley2):
+    if zero % 2:
+        zero = zero * 0
+        mass_1.append(zero)
+
+print(mass_1)
