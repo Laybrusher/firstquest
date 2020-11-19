2f52a3906b3eb5cc8b41ce74388cf86800c1104f
sinsin666 <ssin5912@gmail.com>
2020-11-19 21:35:53 +0900

    заполнение массива

---
+++ array.py
@@ -0,0 +1,9 @@
+mass_1 = []
+volley = input("Введите число ")
+volley2 = int(volley)
+zero = ()
+
+for volley in range(volley2):
+    string = ""
+    if volley % 2:
+        mass_1.insert(0, volley)
